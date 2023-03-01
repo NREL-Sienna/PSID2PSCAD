33f018e0af3695bd26bb71ef6a52f18ae9996775
Matt Bossart <bossart.matt@gmail.com>
2023-03-01 11:02:15 -0500

    progress toward running 144 bus

---
+++ _pscad_psid_conversion/build_system.jl
@@ -0,0 +1,387 @@
+
+bus_length_const = 6
+
+function build_system(sys::System, project, coorDict; add_line_breakers = false, add_multimeters = false)
+    main = project.user_canvas("Main")
+    y_coord = 2
+    for signal in ["t_INV", "t_GEN", "t_L2N", "t_S2M", "t_RAMP"]
+        t_INV_const = main.add_component("master", "const", 3, y_coord)
+        t_INV_const.set_parameters((Name = signal))
+        t_INV = main.add_component("master", "datalabel", 5, y_coord)
+        t_INV.set_parameters(Name = signal)
+        y_coord += 3 
+    end 
+    components = collect(get_components(Component, sys))
+    for c in components
+        @info "building component: $(get_name(c)) of type $(typeof(c))"
+        build_component(c, get_name(c), main, coorDict, sys; add_line_breakers, add_multimeters)
+    end 
+end
+
+function build_component(psid_component::Bus, pscad_component_name, pscad_canvas, coorDict, sys; add_line_breakers = false, add_multimeters = false)
+    BusDict = Dict()
+    for (key, value) in coorDict
+        if value.orientation == "tall"
+            BusDict[key] = (value.centerpoint[1], value.centerpoint[2] - bus_length_const), (value.centerpoint[1], value.centerpoint[2] + bus_length_const)
+        elseif value.orientation == "wide"
+            BusDict[key] = (value.centerpoint[1] - bus_length_const, value.centerpoint[2]), (value.centerpoint[1] + bus_length_const, value.centerpoint[2])
+        else
+            @error "error: not tall or wide"
+        end
+    end
+    new_bus = pscad_canvas.create_bus(
+        BusDict[pscad_component_name][1],
+        BusDict[pscad_component_name][2],
+    )
+    new_bus.parameters(Name = pscad_component_name)
+
+    if add_multimeters == true
+        if coorDict[pscad_component_name].orientation == "tall"
+            multimeter_offset_x = 2
+            multimeter_offset_y = 5 
+        else    
+            multimeter_offset_x = 6
+            multimeter_offset_y = 1 
+        end
+        new_multi = pscad_canvas.add_component(
+            "master",
+            "multimeter",
+            coorDict[pscad_component_name].centerpoint[1] + multimeter_offset_x,
+            coorDict[pscad_component_name].centerpoint[2] + multimeter_offset_y
+            )
+        multi_name = "m_"*pscad_component_name
+        new_multi.set_parameters(
+            Name = multi_name,
+            RMS = 2,    # 2 -> digital measurement 
+            MeasPh = 1, # 1 -> measure in radians 
+            Vrms = "v_"*pscad_component_name,
+            Ph = "ph_"*pscad_component_name,
+            Freq = 30000.0, #frequency for digital measurement 
+            BaseV = get_base_voltage(psid_component)
+            )
+        new_wire = pscad_canvas.add_wire(new_multi.get_port_location("A"), coorDict[pscad_component_name].centerpoint)
+    end
+end
+
+function build_component(
+    psid_component::DynamicGenerator{
+        SauerPaiMachine,
+        SingleMass,
+        SEXS,
+        SteamTurbineGov1,
+        PSSFixed,
+    },
+    pscad_component_name,
+    pscad_canvas,
+    coorDict,
+    sys;
+    add_line_breakers = false,
+    add_multimeters = false
+)
+    static_injector = get_component(StaticInjection, sys, pscad_component_name)
+    Busname = get_name(get_bus(static_injector))
+    if coorDict[Busname].devices_location == "s"
+        new_mach = pscad_canvas.add_component(
+            "PSID_Library_Inverters",
+            "SAUERPAI_SEXS_TGOV1_PSSFIXED",
+            coorDict[Busname].centerpoint[1] - 10,
+            coorDict[Busname].centerpoint[2] + 7,
+        )
+    elseif coorDict[Busname].devices_location == "n"
+        new_mach = pscad_canvas.add_component(
+            "PSID_Library_Inverters",
+            "SAUERPAI_SEXS_TGOV1_PSSFIXED",
+            coorDict[Busname].centerpoint[1] - 10,
+            coorDict[Busname].centerpoint[2] - 7,
+        )
+    elseif coorDict[Busname].devices_location == "e"
+        new_mach = pscad_canvas.add_component(
+            "PSID_Library_Inverters",
+            "SAUERPAI_SEXS_TGOV1_PSSFIXED",
+            coorDict[Busname].centerpoint[1] + 7,
+            coorDict[Busname].centerpoint[2] + 10,
+        )
+    elseif coorDict[Busname].devices_location == "w"
+        new_mach = pscad_canvas.add_component(
+            "PSID_Library_Inverters",
+            "SAUERPAI_SEXS_TGOV1_PSSFIXED",
+            coorDict[Busname].centerpoint[1] - 7,
+            coorDict[Busname].centerpoint[2] + 10,
+        )
+    else
+        @error "No direction specified for component placement"
+        println(Busname)
+    end
+    new_mach.set_parameters(Name = pscad_component_name)
+    new_wire =
+        pscad_canvas.create_wire(new_mach.get_port_location("POI"), coorDict[Busname][1])
+end
+
+function build_component(
+    psid_component::DynamicInverter{
+        AverageConverter,
+        OuterControl{ActivePowerDroop, ReactivePowerDroop},
+        VoltageModeControl,
+        FixedDCSource,
+        FixedFrequency,
+        LCLFilter,
+    },
+    pscad_component_name,
+    pscad_canvas,
+    coorDict,
+    sys;
+    add_line_breakers = false, 
+    add_multimeters = false
+)
+    static_injector = get_component(StaticInjection, sys, pscad_component_name)
+    Busname = get_name(get_bus(static_injector))
+    if coorDict[Busname].devices_location == "s"
+        new_inv = pscad_canvas.add_component(
+            "PSID_Library_Inverters",
+            "DROOP_GFM",
+            coorDict[Busname].centerpoint[1] - 3,
+            coorDict[Busname].centerpoint[2] + 7,
+        )
+    elseif coorDict[Busname].devices_location == "n"
+        new_inv = pscad_canvas.add_component(
+            "PSID_Library_Inverters",
+            "DROOP_GFM",
+            coorDict[Busname].centerpoint[1] - 3,
+            coorDict[Busname].centerpoint[2] - 7,
+        )
+    elseif coorDict[Busname].devices_location == "e"
+        new_inv = pscad_canvas.add_component(
+            "PSID_Library_Inverters",
+            "DROOP_GFM",
+            coorDict[Busname].centerpoint[1] + 7,
+            coorDict[Busname].centerpoint[2] + 5,
+        )
+    elseif coorDict[Busname].devices_location == "w"
+        new_inv = pscad_canvas.add_component(
+            "PSID_Library_Inverters",
+            "DROOP_GFM",
+            coorDict[Busname].centerpoint[1] - 7,
+            coorDict[Busname].centerpoint[2] + 5,
+        )
+    else
+        @error "No direction specified for component placement"
+        println(Busname)
+    end
+    new_inv.set_parameters(Name = pscad_component_name)
+    new_wire =
+        pscad_canvas.create_wire(new_inv.get_port_location("POI"), coorDict[Busname][1])
+end
+
+function build_component(
+    psid_component::DynamicInverter{
+        AverageConverter,
+        OuterControl{ActivePowerPI, ReactivePowerPI},
+        CurrentModeControl,
+        FixedDCSource,
+        KauraPLL,
+        LCLFilter,
+    },
+    pscad_component_name,
+    pscad_canvas,
+    coorDict,
+    sys; 
+    add_line_breakers = false, 
+    add_multimeters = false
+)
+    static_injector = get_component(StaticInjection, sys, pscad_component_name)
+    Busname = get_name(get_bus(static_injector))
+    if coorDict[Busname].devices_location == "s"
+        new_inv = pscad_canvas.add_component(
+            "PSID_Library_Inverters",
+            "GFL_KAURA_PLL",
+            coorDict[Busname].centerpoint[1] + 4,
+            coorDict[Busname].centerpoint[2] + 7,
+        )
+    elseif coorDict[Busname].devices_location == "n"
+        new_inv = pscad_canvas.add_component(
+            "PSID_Library_Inverters",
+            "GFL_KAURA_PLL",
+            coorDict[Busname].centerpoint[1] + 4,
+            coorDict[Busname].centerpoint[2] - 7,
+        )
+    elseif coorDict[Busname].devices_location == "e"
+        new_inv = pscad_canvas.add_component(
+            "PSID_Library_Inverters",
+            "GFL_KAURA_PLL",
+            coorDict[Busname].centerpoint[1] + 7,
+            coorDict[Busname].centerpoint[2],
+        )
+    elseif coorDict[Busname].devices_location == "w"
+        new_inv = pscad_canvas.add_component(
+            "PSID_Library_Inverters",
+            "GFL_KAURA_PLL",
+            coorDict[Busname].centerpoint[1] - 7,
+            coorDict[Busname].centerpoint[2],
+        )
+    else
+        @error "No direction specified for component placement"
+        println(Busname)
+    end
+    new_inv.set_parameters(Name = pscad_component_name)
+    new_wire =
+        pscad_canvas.create_wire(new_inv.get_port_location("POI"), coorDict[Busname][1])
+end
+
+function build_component(psid_component::Line, pscad_component_name, pscad_canvas, coorDict, sys; add_line_breakers = false, add_multimeters = false)
+    split_parts = split(pscad_component_name, "-")
+    midpoint =
+        floor(Int, (coorDict[split_parts[1]].centerpoint[1] + coorDict[split_parts[2]].centerpoint[1]) / 2),
+        floor(Int, (coorDict[split_parts[1]].centerpoint[2] + coorDict[split_parts[2]].centerpoint[2]) / 2)
+    new_pi = pscad_canvas.add_component("master", "newpi", midpoint[1], midpoint[2])
+    new_pi.set_parameters(Name = pscad_component_name)
+
+    if add_line_breakers == true
+        left_br_offset = -3
+        right_br_offset = 3
+        br_coors1 = (midpoint[1]+left_br_offset, midpoint[2])
+        br_coors2 = (midpoint[1]+right_br_offset, midpoint[2])
+        new_br1 = pscad_canvas.add_component(
+            "master",
+            "breaker3",
+            br_coors1[1],
+            br_coors1[2]
+            )
+        new_br2 = pscad_canvas.add_component(
+            "master",
+            "breaker3",
+            br_coors2[1],
+            br_coors2[2]
+            )
+        if coorDict[split_parts[2]].centerpoint[1] > coorDict[split_parts[1]].centerpoint[1]
+            new_wire3 = pscad_canvas.add_wire(coorDict[split_parts[1]].centerpoint, new_br1.get_port_location("N2"))
+            new_wire4 = pscad_canvas.add_wire(coorDict[split_parts[2]].centerpoint, new_br2.get_port_location("N1"))
+        elseif coorDict[split_parts[2]].centerpoint[1] < coorDict[split_parts[1]].centerpoint[1] 
+            new_wire3 = pscad_canvas.add_wire(coorDict[split_parts[2]].centerpoint, new_br1.get_port_location("N2"))
+            new_wire4 = pscad_canvas.add_wire(coorDict[split_parts[1]].centerpoint, new_br2.get_port_location("N1"))
+        else 
+            bus1_left_end = (coorDict[split_parts[1]].centerpoint[1]-bus_length_const, coorDict[split_parts[1]].centerpoint[2])
+            bus2_right_end = (coorDict[split_parts[2]].centerpoint[1]+bus_length_const, coorDict[split_parts[2]].centerpoint[2])
+            new_wire3 = pscad_canvas.add_wire(bus1_left_end, new_br1.get_port_location("N2")) 
+            new_wire4 = pscad_canvas.add_wire(bus2_right_end, new_br2.get_port_location("N1"))
+        end
+        br1_name =  "br_"*replace(pscad_component_name, "-" => "_")*"_1"
+        br2_name =  "br_"*replace(pscad_component_name, "-" => "_")*"_2"
+        new_br1.set_parameters(NAME = br1_name)        #TODO - should give these meaningful names based on :from and :to in PSID
+        new_br2.set_parameters(NAME = br2_name)
+        _add_breaker_logic_peripherals(pscad_canvas, br1_name, br_coors1)
+        _add_breaker_logic_peripherals(pscad_canvas, br2_name, br_coors2)
+    else
+        if coorDict[split_parts[2]].centerpoint[1] > coorDict[split_parts[1]].centerpoint[1]
+            new_wire =
+            pscad_canvas.add_wire(new_pi.get_port_location("N2"), coorDict[split_parts[2]].centerpoint)
+            new_wire2 =
+            pscad_canvas.add_wire(new_pi.get_port_location("N1"), coorDict[split_parts[1]].centerpoint)
+        else
+            new_wire =
+            pscad_canvas.add_wire(new_pi.get_port_location("N1"), coorDict[split_parts[2]].centerpoint)
+            new_wire2 =
+            pscad_canvas.add_wire(new_pi.get_port_location("N2"), coorDict[split_parts[1]].centerpoint)
+        end
+    end
+end
+
+function _add_breaker_logic_peripherals(pscad_canvas, br_name, br_coord)
+    brk_logic = pscad_canvas.add_component("PSID_Library", "simple_brk_logic", br_coord[1], br_coord[2] + 3)
+    brk_logic.set_parameters(Name = br_name)
+    brk_logic.set_parameters(t_break = 0.0)
+    label = pscad_canvas.add_component("master", "datalabel", br_coord[1], br_coord[2] + 1)
+    label.set_parameters(Name = br_name)
+end 
+
+function build_component(
+    psid_component::Transformer2W,
+    pscad_component_name,
+    pscad_canvas,
+    coorDict,
+    sys; 
+    add_line_breakers = false, 
+    add_multimeters = false
+)
+    split_parts = split(pscad_component_name, "-")
+    midpoint =
+        floor(Int, (coorDict[split_parts[1]].centerpoint[1] + coorDict[split_parts[2]].centerpoint[1]) / 2),
+        floor(Int, (coorDict[split_parts[1]].centerpoint[2] + coorDict[split_parts[2]].centerpoint[2]) / 2)
+    new_xfmr = pscad_canvas.add_component("master", "xfmr-3p2w", midpoint[1], midpoint[2])
+    new_xfmr.set_parameters(Name = pscad_component_name)
+    if coorDict[split_parts[2]].centerpoint[1] > coorDict[split_parts[1]].centerpoint[1]
+        new_wire =
+        pscad_canvas.add_wire(new_xfmr.get_port_location("N2"), coorDict[split_parts[2]].centerpoint)
+        new_wire2 =
+        pscad_canvas.add_wire(new_xfmr.get_port_location("N1"), coorDict[split_parts[1]].centerpoint)
+    else
+        new_wire =
+        pscad_canvas.add_wire(new_xfmr.get_port_location("N1"), coorDict[split_parts[2]].centerpoint)
+        new_wire2 =
+        pscad_canvas.add_wire(new_xfmr.get_port_location("N2"), coorDict[split_parts[1]].centerpoint)
+    end
+end
+
+function build_component(
+    psid_component::PowerLoad,
+    pscad_component_name,
+    pscad_canvas,
+    coorDict,
+    sys; 
+    add_line_breakers = false, 
+    add_multimeters = false
+)
+    loadbus = get_name(get_bus(psid_component))
+    new_load = pscad_canvas.add_component(
+        "master",
+        "fixed_load",
+        coorDict[loadbus].centerpoint[1] + 2,
+        coorDict[loadbus].centerpoint[2] + 2,
+    )
+    new_load.set_parameters(Name = pscad_component_name)
+    new_wire =
+        pscad_canvas.create_wire(new_load.get_port_location("IA"), coorDict[loadbus][1])
+end
+
+function build_component(psid_component::Arc, pscad_component_name, pscad_canvas, coorDict, sys; add_line_breakers = false, add_multimeters = false)
+    @info "Skipping build for type Arc"
+end
+
+function build_component(
+    psid_component::LoadZone,
+    pscad_component_name,
+    pscad_canvas,
+    coorDict,
+    sys; 
+    add_line_breakers = false, 
+    add_multimeters = false
+)
+    @info "Skipping build for type LoadZone"
+end
+
+function build_component(psid_component::Area, pscad_component_name, pscad_canvas, coorDict, sys; add_line_breakers = false, add_multimeters = false)
+    @info "Skipping build for type Area"
+end
+
+function build_component(
+    psid_component::GenericBattery,
+    pscad_component_name,
+    pscad_canvas,
+    coorDict,
+    sys; 
+    add_line_breakers = false, 
+    add_multimeters = false
+)
+    @info "Skipping build for type GenericBattery"
+end
+
+function build_component(
+    psid_component::ThermalStandard,
+    pscad_component_name,
+    pscad_canvas,
+    coorDict,
+    sys; 
+    add_line_breakers = false, 
+    add_multimeters = false
+)
+    @info "Skipping build for type ThermalStandard"
+end
---
+++ _pscad_psid_conversion/simulation_extras.jl
@@ -0,0 +1,173 @@
+function pscad_compat_name(psid_name)
+    return replace(psid_name, "-" => "_")
+end 
+
+function setup_breaker_operations(project, perturbation::PowerSimulationsDynamics.BranchTrip)
+    breaker_name =string("br_", pscad_compat_name(perturbation.branch_name), "_1") 
+    breaker_logic = project.find("PSID_Library:simple_brk_logic", breaker_name )
+    breaker_logic.set_parameters(enable = 1, t_break = 1.0)
+end  
+
+function setup_output_channnels(project, quantities_to_record::Vector{Tuple{Symbol, String}}, starting_coord; force = true)
+    pscad_canvas = project.user_canvas("Main")
+    if force == true 
+        #TODO: find_output_channels on the main canvas and delete them along with any connected component (if possible)
+    end
+    for q in quantities_to_record
+        channel_name = string(q[1], "_", q[2])
+        new_channel = pscad_canvas.add_component(
+            "master",
+            "pgb",
+            starting_coord[1],
+            starting_coord[2]
+            )
+        new_signal = pscad_canvas.add_component(
+            "master",
+            "datalabel",
+            starting_coord[1],
+            starting_coord[2]
+            )
+    
+        #new_channel.set_parameters(Name = channel_name)
+        new_signal.set_parameters(Name = channel_name)
+        starting_coord = (starting_coord[1] + 5, starting_coord[2])
+    end 
+end 
+
+function set_project_parameters!(project; kwargs...)
+    project_params = project.parameters()
+    for k in kwargs
+        project_params[string(k[1])] = k[2]
+    end
+    PP.update_parameter_by_dictionary(project, project_params)
+end
+
+#TODO - run the three bus tests by re-building each system instead of messing with layers. 
+#once tests pass, remove all the layer based functions.
+function _add_to_enabled_gens_layer(
+    g::DynamicInverter{
+        AverageConverter,
+        OuterControl{VirtualInertia, ReactivePowerDroop},
+        VoltageModeControl,
+        FixedDCSource,
+        KauraPLL,
+        LCLFilter,
+    },
+    project,
+)
+    psid_name = get_name(g)
+    project.find("PSID_Library_Inverters:DARCO_VSM", psid_name).add_to_layer("enabled_gens")
+end
+
+function _add_to_enabled_gens_layer(
+    g::DynamicInverter{
+        AverageConverter,
+        OuterControl{ActivePowerDroop, ReactivePowerDroop},
+        VoltageModeControl,
+        FixedDCSource,
+        FixedFrequency,
+        LCLFilter,
+    },
+    project,
+)
+    psid_name = get_name(g)
+    project.find("PSID_Library_Inverters:DROOP_GFM", psid_name).add_to_layer("enabled_gens")
+end
+
+function _add_to_enabled_gens_layer(
+    g::DynamicInverter{
+        AverageConverter,
+        OuterControl{ActivePowerPI, ReactivePowerPI},
+        CurrentModeControl,
+        FixedDCSource,
+        ReducedOrderPLL,
+        LCLFilter,
+    },
+    project,
+)
+    psid_name = get_name(g)
+    project.find("PSID_Library_Inverters:GFL", psid_name).add_to_layer("enabled_gens")
+end
+
+function _add_to_enabled_gens_layer(
+    g::DynamicGenerator{SauerPaiMachine, SingleMass, SEXS, GasTG, IEEEST},
+    project,
+)
+    psid_name = get_name(g)
+    project.find("PSID_Library_Inverters:SAUERPAI_SEXS_GASTG_IEEEST", psid_name).add_to_layer("enabled_gens")
+end
+
+function _add_to_enabled_gens_layer(
+    g::DynamicGenerator{SauerPaiMachine, SingleMass, SEXS, GasTG, PSSFixed},
+    project,
+)
+    psid_name = get_name(g)
+    project.find("PSID_Library_Inverters:SAUERPAI_SEXS_GASTG_PSSFIXED", psid_name).add_to_layer("enabled_gens")
+end
+
+function _add_to_enabled_gens_layer(
+    g::DynamicGenerator{SauerPaiMachine, SingleMass, SEXS, HydroTurbineGov, IEEEST},
+    project,
+)
+    psid_name = get_name(g)
+    project.find("PSID_Library_Inverters:SAUERPAI_SEXS_HYGOV_IEEEST", psid_name).add_to_layer("enabled_gens")
+end
+
+function _add_to_enabled_gens_layer(
+    g::DynamicGenerator{SauerPaiMachine, SingleMass, SEXS, HydroTurbineGov, PSSFixed},
+    project,
+)
+    psid_name = get_name(g)
+    project.find("PSID_Library_Inverters:SAUERPAI_SEXS_HYGOV_PSSFIXED", psid_name).add_to_layer("enabled_gens")
+end
+
+function _add_to_enabled_gens_layer(
+    g::DynamicGenerator{SauerPaiMachine, SingleMass, SEXS, SteamTurbineGov1, IEEEST},
+    project,
+)
+    psid_name = get_name(g)
+    project.find("PSID_Library_Inverters:SAUERPAI_SEXS_TGOV1_IEEEST", psid_name).add_to_layer("enabled_gens")
+end
+
+function _add_to_enabled_gens_layer(
+    g::DynamicGenerator{SauerPaiMachine, SingleMass, SEXS, SteamTurbineGov1, PSSFixed},
+    project,
+)
+    psid_name = get_name(g)
+    project.find("PSID_Library_Inverters:SAUERPAI_SEXS_TGOV1_PSSFIXED", psid_name).add_to_layer("enabled_gens")
+end
+
+function _add_to_enabled_gens_layer(
+    g::DynamicGenerator{SauerPaiMachine, SingleMass, AVRFixed, TGFixed, PSSFixed},
+    project,
+)
+    psid_name = get_name(g)
+    project.find("PSID_Library_Inverters:FIXED_SAUER_PAI", psid_name).add_to_layer(
+        "enabled_gens",
+    )
+    #project.find("PSID_Library_Inverters:SIMPLE_MACHINE", psid_name).add_to_layer("enabled_gens")           #SWITCH HERE TO DECIDE WHICH MACHINE MODEL 
+end
+
+function _add_to_enabled_gens_layer(g::Source, project)
+    psid_name = get_name(g)
+    project.find("PSID_Library_Inverters:INFINITE_BUS", psid_name).add_to_layer(
+        "enabled_gens",
+    )
+end
+
+function enable_dynamic_injection_by_type(sys, project)
+    for g in get_components(DynamicInjection, sys)
+        psid_name = get_name(g)
+        for f in project.find_all(psid_name)
+            f.add_to_layer("disabled_gens")
+        end
+        _add_to_enabled_gens_layer(g, project)
+    end
+    for g in get_components(Source, sys)
+        psid_name = get_name(g)
+        for f in project.find_all(psid_name)
+            f.add_to_layer("disabled_gens")
+        end
+        _add_to_enabled_gens_layer(g, project)
+    end
+end
